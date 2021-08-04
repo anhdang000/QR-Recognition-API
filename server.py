01f404e44cfb29188082e46cc215195a7014478f
anhdang000 <danglinh.anh000@gmail.com>
2021-08-04 13:24:37 +0700

    Update server.py

--- server.py
+++ server.py
@@ -20,18 +20,32 @@ reader = pyzxing.BarCodeReader()
 app = FastAPI()
 
 CACHE_DIR = '.cache'
+if not os.path.isdir(CACHE_DIR):
+        os.mkdir(CACHE_DIR)
 
 @app.post('/read-extracted-qr')
 async def read_extracted_qr(qr_image: UploadFile = File(...)):
-    if not os.path.isdir(CACHE_DIR):
-        os.mkdir(CACHE_DIR)
+    # Check file format
+    file_ext = qr_image.filename.split('.')[-1]
+    supported_image_formats = ['bmp', 'jpg', 'jpeg', 'jp2', 'png', 'tiff', 'webp', 'xbm']
+    if file_ext not in supported_image_formats:
+            return {
+                "error": "invalid input"
+                }
+    try:
+        image = Image.open(qr_image.file).convert('RGB')
+        if image is None:
+            return {"error": "invalid input"}
+    except:
+        return {"error": "invalid input"}
+
     save_path = join(CACHE_DIR, qr_image.filename.split('.')[0] + '_' + '_'.join(str(datetime.now()).split()) + '.png')
     await chunked_copy(qr_image, save_path)
 
     org_img = cv2.imread(save_path)
     h, w = org_img.shape if len(org_img.shape) == 2 else org_img.shape[:2]
 
-    scale = 800/w
+    scale = round(800/w)
     coeff = -1/2
     if max(h, w) < 800:
         img = cv2.resize(org_img, None, fx=scale, fy=scale)
@@ -47,6 +61,7 @@ async def read_extracted_qr(qr_image: UploadFile = File(...)):
     if len(qr_result) > 1:
         return parse_result_into_fields(qr_result)
     else:
-        img = bicubic(org_img, scale, coeff)
+        img = bicubic(org_img, scale/2, coeff)
+        img = bicubic(img, scale/2, coeff)
         qr_result = reader.decode_array(img)[0]
         return parse_result_into_fields(qr_result)
