43b7da407fa8c712aaa73b6d724a0c4528b5b48a
anhdang000 <danglinh.anh000@gmail.com>
2021-08-04 14:59:28 +0700

    Bugfix

--- server.py
+++ server.py
@@ -45,23 +45,24 @@ async def read_extracted_qr(qr_image: UploadFile = File(...)):
     org_img = cv2.imread(save_path)
     h, w = org_img.shape if len(org_img.shape) == 2 else org_img.shape[:2]
 
-    scale = round(800/w)
+    scale = 800 / w
     coeff = -1/2
     if max(h, w) < 800:
         img = cv2.resize(org_img, None, fx=scale, fy=scale)
 
-    img = cv2.cvtColor(org_img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = adjust_image_gamma_lookuptable(img, 0.6)
     img = np.clip(img*1.7, 0, 255)
     img = cv2.GaussianBlur(img, (5,5), 0)
     img = img.astype(np.uint8)
     _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # cv2.imwrite(save_path, img)
     qr_result = reader.decode_array(img)[0]
     
     if len(qr_result) > 1:
         return parse_result_into_fields(qr_result)
     else:
         img = bicubic(org_img, scale/2, coeff)
-        img = bicubic(img, scale/2, coeff)
+        # img = bicubic(img, scale/2, coeff)
         qr_result = reader.decode_array(img)[0]
         return parse_result_into_fields(qr_result)
